7916c6f0a6659d4e8a37840031eb8b18c952cda9
V-S3V3N <thamqilun@gmail.com>
2024-08-03 22:35:39 +0800

    up

--- CoffeeCove/Order/orderOption.aspx.cs
+++ CoffeeCove/Order/orderOption.aspx.cs
@@ -7,16 +7,15 @@ using System.Web.UI.WebControls;
 
 namespace CoffeeCove.Order
 {
-    public partial class orderOpt : System.Web.UI.Page
+    public partial class orderOption : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+
         }
         protected void lbGurney_Click(object sender, EventArgs e)
         {
             litStore.Text = "Gurney Plaza";
         }
-
     }
 }
