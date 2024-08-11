35d89966adacf3428ece4e3a43b79b528145fba6
Thomas Lim <90422186+thomaslimfc@users.noreply.github.com>
2024-08-12 01:08:33 +0800

    user profile upload profile picture

--- CoffeeCove/UserManagement/UserProfile.aspx
+++ CoffeeCove/UserManagement/UserProfile.aspx
@@ -12,8 +12,26 @@
     
     <center>
         <div class="profileContainer">
-        <br />
+        <br /> 
         <table style="user-select: none">
+            <tr>
+                <td class="blankCol"></td>
+                <td class="blankCol" colspan="4">
+                    <asp:Image ID="imgProfilePicture" runat="server" Width="150px" Height="150px" />
+                    <br />
+                    <asp:FileUpload ID="fuProfilePicture" runat="server"
+                        style="margin-top: 10px" />
+                    <br />
+                    <asp:Button ID="UploadPictureBtn_UP" runat="server" Text="Upload Picture" 
+                        style="margin-top: 10px"
+                        CssClass="btn btn-primary" OnClick="UploadPictureBtn_UP_Click" />
+                    <br />
+                    <asp:Label ID="lblUploadMessage" runat="server"
+                        CssClass="text-success" Visible="false"></asp:Label>
+                </td>
+                <td class="blankCol"></td>
+            </tr>
+
             <tr><td><br /></td></tr>
             <tr>
                 <td class="blankCol"></td>
@@ -30,27 +48,31 @@
             </tr>
             <tr class="evenRow">
                 <td class="blankCol"></td>
-                <td class="contentCol">
+                <td class="contentCol" style="height: 40px">
                     <asp:Label ID="lblUsername" runat="server" />
-                    <asp:TextBox ID="txtUsername" runat="server" Visible="false" />
+                    <asp:TextBox ID="txtUsername" runat="server" 
+                        CssClass="SizeEditTextbox" Visible="false" />
                     <asp:RequiredFieldValidator 
                         ID="Username_rqdValidator" runat="server" 
                         ControlToValidate="txtUsername" 
                         ErrorMessage="Username is required." 
                         Display="Dynamic" 
                         ForeColor="Red" 
-                        CssClass="rqdValidator" />
+                        CssClass="rqdValidator"
+                        ValidationGroup="SaveProfile" />
                 </td>
                 <td class="contentCol">
                     <asp:Label ID="lblGender" runat="server" />
-                    <asp:TextBox ID="txtGender" runat="server" Visible="false" />
+                    <asp:TextBox ID="txtGender" runat="server" 
+                        CssClass="SizeEditTextbox" Visible="false" />
                     <asp:RequiredFieldValidator 
                         ID="Gender_rqdValidator" runat="server" 
                         ControlToValidate="txtGender" 
                         ErrorMessage="Gender is required." 
                         Display="Dynamic" 
                         ForeColor="Red" 
-                        CssClass="rqdValidator" />
+                        CssClass="rqdValidator"
+                        ValidationGroup="SaveProfile" />
                 </td>
                 <td class="blankCol"></td>
             </tr>
@@ -64,16 +86,18 @@
             </tr>
             <tr class="evenRow">
                 <td class="blankCol"></td>
-                <td class="contentCol">
+                <td class="contentCol" style="height: 40px">
                     <asp:Label ID="lblEmail" runat="server" />
-                    <asp:TextBox ID="txtEmail" runat="server" Visible="false" />
+                    <asp:TextBox ID="txtEmail" runat="server" 
+                        CssClass="SizeEditTextbox" Visible="false" />
                     <asp:RequiredFieldValidator 
                         ID="Email_rqdValidator" runat="server" 
                         ControlToValidate="txtEmail" 
                         ErrorMessage="Email Address is required." 
                         Display="Dynamic" 
                         ForeColor="Red" 
-                        CssClass="rqdValidator" />
+                        CssClass="rqdValidator"
+                        ValidationGroup="SaveProfile" />
                     <asp:RegularExpressionValidator 
                         ID="Email_regexValidator" runat="server" 
                         ControlToValidate="txtEmail" 
@@ -81,18 +105,21 @@
                         Display="Dynamic" 
                         ForeColor="Red" 
                         CssClass="rqdValidator" 
-                        ValidationExpression="^[^@\s]+@[^@\s]+\.[^@\s]+$" />
+                        ValidationExpression="^[^@\s]+@[^@\s]+\.[^@\s]+$"
+                        ValidationGroup="SaveProfile" />
                 </td>
                 <td class="contentCol">
                     <asp:Label ID="lblDOB" runat="server" />
-                    <asp:TextBox ID="txtDOB" runat="server" Visible="false" />
+                    <asp:TextBox ID="txtDOB" runat="server" 
+                        CssClass="SizeEditTextbox" Visible="false" />
                     <asp:RequiredFieldValidator 
                         ID="DOB_rqdValidator" runat="server" 
                         ControlToValidate="txtDOB" 
                         ErrorMessage="Date of Birth is required." 
                         Display="Dynamic" 
                         ForeColor="Red" 
-                        CssClass="rqdValidator" />
+                        CssClass="rqdValidator"
+                        ValidationGroup="SaveProfile" />
                     <asp:CompareValidator 
                         ID="DOB_compareValidator" runat="server" 
                         ControlToValidate="txtDOB" 
@@ -100,7 +127,8 @@
                         Display="Dynamic" 
                         ForeColor="Red" 
                         CssClass="rqdValidator" 
-                        Operator="DataTypeCheck" Type="Date" />
+                        Operator="DataTypeCheck" Type="Date" 
+                        ValidationGroup="SaveProfile" />
                 </td>
                 <td class="blankCol"></td>
             </tr>
@@ -114,49 +142,54 @@
             </tr>
             <tr class="evenRow">
                 <td class="blankCol"></td>
-                <td class="contentCol">
+                <td class="contentCol" style="height: 40px">
                     <asp:Label ID="lblContactNo" runat="server" />
-                    <asp:TextBox ID="txtContactNo" runat="server" Visible="false" />
+                    <asp:TextBox ID="txtContactNo" runat="server" 
+                        CssClass="SizeEditTextbox" Visible="false" />
                     <asp:RequiredFieldValidator 
                         ID="ContactNo_rqdValidator" runat="server" 
                         ControlToValidate="txtContactNo" 
                         ErrorMessage="Contact Number is required." 
                         Display="Dynamic" 
                         ForeColor="Red" 
-                        CssClass="rqdValidator" />
+                        CssClass="rqdValidator"
+                        ValidationGroup="SaveProfile" />
                 </td>
                 <td class="contentCol">
                     <asp:Label ID="lblResidenceState" runat="server" />
-                    <asp:TextBox ID="txtResidenceState" runat="server" Visible="false" />
+                    <asp:TextBox ID="txtResidenceState" runat="server" 
+                        CssClass="SizeEditTextbox" Visible="false" />
                     <asp:RequiredFieldValidator 
                         ID="ResidenceState_rqdValidator" runat="server" 
                         ControlToValidate="txtResidenceState" 
                         ErrorMessage="Residence State is required." 
                         Display="Dynamic" 
                         ForeColor="Red" 
-                        CssClass="rqdValidator" />
+                        CssClass="rqdValidator"
+                        ValidationGroup="SaveProfile" />
                 </td>
                 <td class="blankCol"></td>
             </tr>
             <tr><td><br /></td></tr>
         <tr>
             <td colspan="4">
-                <asp:Button ID="btnEdit" runat="server" 
+                <asp:Button ID="EditBtn_UP" runat="server" 
                     Text="Edit Info" CssClass="btn btn-success"
                     style="width: 150px;"
-                    OnClick="btnEdit_Click" />
-                <asp:Button ID="btnSave" runat="server" 
-                    Text="Save Now" CssClass="btn btn-danger"
-                    style="width: 150px;"
-                    OnClick="btnSave_Click" Visible="false" />
+                    OnClick="EditBtn_UP_Click"
+                    CausesValidation="false" />
+                <asp:Button ID="SaveBtn_UP" runat="server" 
+                    Text="Save Now" CssClass="btn btn-primary" 
+                    style="width: 150px;" Visible="false" 
+                    OnClick="SaveBtn_UP_Click"
+                    CausesValidation="true" />
             </td>
         </tr>
         <tr>
             <td class="trMarginBottom20"></td>
         </tr>
-        </table>
-        <br />
-        </div>
+    </table>
+    </div>
     </center>
-
+    <br /><br /><br />
 </asp:Content>
--- CoffeeCove/UserManagement/UserProfile.aspx.cs
+++ CoffeeCove/UserManagement/UserProfile.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Data;
 using System.Data.SqlClient;
+using System.Configuration;
 
 namespace CoffeeCove.UserManagement
 {
@@ -9,106 +11,180 @@ namespace CoffeeCove.UserManagement
         {
             if (!IsPostBack)
             {
+                Session["Username"] = "xylim2002";
+
                 LoadUserProfile();
+                SetProfileEditMode(false);
             }
         }
 
-        private void LoadUserProfile()
+        protected void LoadUserProfile()
         {
-            // Retrieve the logged-in user's username
-            string username = "xylim2002";
-
-            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["CoffeeCoveDB"].ConnectionString;
-            string query = "SELECT Username, Gender, EmailAddress, DateOfBirth, ContactNo, ResidenceState FROM Customer WHERE Username = @Username";
-
-            using (SqlConnection con = new SqlConnection(connectionString))
+            string username = Session["Username"]?.ToString();
+            if (!string.IsNullOrEmpty(username))
             {
-                using (SqlCommand cmd = new SqlCommand(query, con))
+                string connectionString = ConfigurationManager.ConnectionStrings["CoffeeCoveDB"].ConnectionString;
+
+                using (SqlConnection con = new SqlConnection(connectionString))
                 {
+                    string query = "SELECT EmailAddress, Gender, DateOfBirth, ContactNo, ResidenceState FROM [dbo].[Customer] WHERE Username = @Username";
+                    SqlCommand cmd = new SqlCommand(query, con);
                     cmd.Parameters.AddWithValue("@Username", username);
 
-                    con.Open();
-                    SqlDataReader reader = cmd.ExecuteReader();
-                    if (reader.Read())
+                    try
                     {
-                        lblUsername.Text = reader["Username"].ToString();
-                        lblGender.Text = reader["Gender"].ToString();
-                        lblEmail.Text = reader["EmailAddress"].ToString();
+                        con.Open();
+                        SqlDataReader reader = cmd.ExecuteReader();
 
-                        DateTime dob = Convert.ToDateTime(reader["DateOfBirth"]);
-                        lblDOB.Text = dob.ToString("d");
+                        if (reader.HasRows)
+                        {
+                            reader.Read();
 
-                        lblContactNo.Text = reader["ContactNo"].ToString();
-                        lblResidenceState.Text = reader["ResidenceState"].ToString();
-
-                        // Set TextBox values
-                        txtUsername.Text = lblUsername.Text;
-                        txtGender.Text = lblGender.Text;
-                        txtEmail.Text = lblEmail.Text;
-                        txtDOB.Text = lblDOB.Text;
-                        txtContactNo.Text = lblContactNo.Text;
-                        txtResidenceState.Text = lblResidenceState.Text;
+                            lblUsername.Text = username;
+                            lblEmail.Text = reader["EmailAddress"].ToString();
+                            lblGender.Text = reader["Gender"].ToString();
+                            lblDOB.Text = Convert.ToDateTime(reader["DateOfBirth"]).ToString("yyyy-MM-dd");
+                            lblContactNo.Text = reader["ContactNo"].ToString();
+                            lblResidenceState.Text = reader["ResidenceState"].ToString();
+                        }
+                        else
+                        {
+                            // Handle case where the username is not found
+                            lblUsername.Text = "Username not found.";
+                        }
                     }
-                    con.Close();
+                    catch (Exception ex)
+                    {
+                        // Handle any errors that may have occurred
+                        lblUsername.Text = "An error occurred: " + ex.Message;
+                    }
                 }
             }
-        }
-
-        protected void btnEdit_Click(object sender, EventArgs e)
-        {
-            ToggleControls(false);
-        }
-
-        protected void btnSave_Click(object sender, EventArgs e)
-        {
-            string username = "xylim2002";  // The logged-in user's username, replace with the actual logged-in user logic
-            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["CoffeeCoveDB"].ConnectionString;
-
-            string query = "UPDATE Customer SET Gender = @Gender, EmailAddress = @EmailAddress, DateOfBirth = @DateOfBirth, ContactNo = @ContactNo, ResidenceState = @ResidenceState WHERE Username = @Username";
-
-            using (SqlConnection con = new SqlConnection(connectionString))
+            else
             {
-                using (SqlCommand cmd = new SqlCommand(query, con))
+                // Handle case where the username is null or empty
+                lblUsername.Text = "Username is not provided.";
+            }
+        }
+
+        protected void SetProfileEditMode(bool isEditMode)
+        {
+            if (isEditMode)
+            {
+                // Populate the text boxes with current values
+                txtUsername.Text = lblUsername.Text;
+                txtEmail.Text = lblEmail.Text;
+                txtGender.Text = lblGender.Text;
+                txtDOB.Text = lblDOB.Text;
+                txtContactNo.Text = lblContactNo.Text;
+                txtResidenceState.Text = lblResidenceState.Text;
+            }
+
+            // Toggle visibility between labels and text boxes
+            lblUsername.Visible = !isEditMode;
+            txtUsername.Visible = isEditMode;
+
+            lblEmail.Visible = !isEditMode;
+            txtEmail.Visible = isEditMode;
+
+            lblGender.Visible = !isEditMode;
+            txtGender.Visible = isEditMode;
+
+            lblDOB.Visible = !isEditMode;
+            txtDOB.Visible = isEditMode;
+
+            lblContactNo.Visible = !isEditMode;
+            txtContactNo.Visible = isEditMode;
+
+            lblResidenceState.Visible = !isEditMode;
+            txtResidenceState.Visible = isEditMode;
+
+            EditBtn_UP.Visible = !isEditMode;
+            SaveBtn_UP.Visible = isEditMode;
+        }
+
+
+        protected void EditBtn_UP_Click(object sender, EventArgs e)
+        {
+            SetProfileEditMode(true);
+        }
+
+        protected void SaveBtn_UP_Click(object sender, EventArgs e)
+        {
+            // Manually trigger validation
+            Page.Validate("SaveProfile");
+
+            if (Page.IsValid)
+            {
+                UpdateUserProfile(); // Call the method to save the updated profile
+
+                // Reload the profile after saving to refresh the labels
+                LoadUserProfile();
+
+                // Redirect back to profile view mode after saving
+                SetProfileEditMode(false);
+            }
+            else
+            {
+                // Keep in edit mode if validation fails
+                SetProfileEditMode(true);
+            }
+        }
+
+        protected void UpdateUserProfile()
+        {
+            string username = Session["Username"]?.ToString();
+            if (!string.IsNullOrEmpty(username))
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["CoffeeCoveDB"].ConnectionString;
+
+                using (SqlConnection con = new SqlConnection(connectionString))
                 {
-                    cmd.Parameters.AddWithValue("@Username", username);
-                    cmd.Parameters.AddWithValue("@Gender", txtGender.Text);
+                    string query = "UPDATE [dbo].[Customer] SET EmailAddress = @EmailAddress, Gender = @Gender, " +
+                                   "DateOfBirth = @DateOfBirth, ContactNo = @ContactNo, ResidenceState = @ResidenceState " +
+                                   "WHERE Username = @Username";
+
+                    SqlCommand cmd = new SqlCommand(query, con);
                     cmd.Parameters.AddWithValue("@EmailAddress", txtEmail.Text);
+                    cmd.Parameters.AddWithValue("@Gender", txtGender.Text);
                     cmd.Parameters.AddWithValue("@DateOfBirth", Convert.ToDateTime(txtDOB.Text));
                     cmd.Parameters.AddWithValue("@ContactNo", txtContactNo.Text);
                     cmd.Parameters.AddWithValue("@ResidenceState", txtResidenceState.Text);
+                    cmd.Parameters.AddWithValue("@Username", username);
 
-                    con.Open();
-                    cmd.ExecuteNonQuery();
-                    con.Close();
+                    try
+                    {
+                        con.Open();
+                        cmd.ExecuteNonQuery(); // Execute the update query
+                    }
+                    catch (Exception ex)
+                    {
+                        // Handle any errors that may have occurred
+                        lblUsername.Text = "An error occurred: " + ex.Message;
+                    }
                 }
             }
-
-            // After saving, revert to label display
-            ToggleControls(true);
-
-            // Reload the profile to reflect updated information
-            LoadUserProfile();
         }
 
-
-        private void ToggleControls(bool isLabelVisible)
+        protected void UploadPictureBtn_UP_Click(object sender, EventArgs e)
         {
-            lblUsername.Visible = isLabelVisible;
-            lblGender.Visible = isLabelVisible;
-            lblEmail.Visible = isLabelVisible;
-            lblDOB.Visible = isLabelVisible;
-            lblContactNo.Visible = isLabelVisible;
-            lblResidenceState.Visible = isLabelVisible;
+            if (fuProfilePicture.HasFile)
+            {
+                string filename = System.IO.Path.GetFileName(fuProfilePicture.PostedFile.FileName);
+                fuProfilePicture.SaveAs(Server.MapPath("~/UserManagement/UserProfilePictures/") + filename);
+                imgProfilePicture.ImageUrl = "/UserProfilePictures/" + filename;
 
-            txtUsername.Visible = !isLabelVisible;
-            txtGender.Visible = !isLabelVisible;
-            txtEmail.Visible = !isLabelVisible;
-            txtDOB.Visible = !isLabelVisible;
-            txtContactNo.Visible = !isLabelVisible;
-            txtResidenceState.Visible = !isLabelVisible;
-
-            btnEdit.Visible = isLabelVisible;
-            btnSave.Visible = !isLabelVisible;
+                // Show success message
+                lblUploadMessage.Text = "Picture has been successfully uploaded.";
+                lblUploadMessage.Visible = true;
+            }
+            else
+            {
+                lblUploadMessage.Text = "Please select a picture to upload.";
+                lblUploadMessage.CssClass = "text-danger";
+                lblUploadMessage.Visible = true;
+            }
         }
+
     }
 }
