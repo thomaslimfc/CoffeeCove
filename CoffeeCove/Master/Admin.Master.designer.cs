2c620a7d9b40fdb72a0b156cdc9ce07106eb117b
Karenwong0525 <162662722+Karenwong0525@users.noreply.github.com>
2024-08-11 12:31:17 +0800

    b

--- CoffeeCove/Master/Admin.Master.designer.cs
+++ CoffeeCove/Master/Admin.Master.designer.cs
@@ -24,13 +24,13 @@ namespace CoffeeCove.Master
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// litUserName control.
+        /// litUser control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Literal litUserName;
+        protected global::System.Web.UI.WebControls.Literal litUser;
 
         /// <summary>
         /// sideBar control.
