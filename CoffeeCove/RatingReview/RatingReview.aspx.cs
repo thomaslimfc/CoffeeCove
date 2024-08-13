3a324c190b645e542fa7e9e31cf0ac33c349d08a
welxen <limls-pm21@student.tarc.edu.my>
2024-08-13 21:56:29 +0800

    rating

--- CoffeeCove/RatingReview/RatingReview.aspx.cs
+++ CoffeeCove/RatingReview/RatingReview.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace CoffeeCove.RatingReview
 {
-    public partial class RatingReview : System.Web.UI.Page
+    public partial class ratingReview : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
