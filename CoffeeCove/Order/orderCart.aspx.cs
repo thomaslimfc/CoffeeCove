ad16fdfe0fedd26f8dbfc03e7e9daa5d7d00da5c
V-S3V3N <thamqilun@gmail.com>
2024-08-14 20:47:18 +0800

    up

--- CoffeeCove/Order/orderCart.aspx.cs
+++ CoffeeCove/Order/orderCart.aspx.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
+using System.Data;
+using System.Drawing;
 
 namespace CoffeeCove.Order
 {
@@ -16,7 +18,24 @@ namespace CoffeeCove.Order
         {
             if (!Page.IsPostBack)
             {
-                string sql = "Select";
+                string id = Request.QueryString["id"] ?? "";
+
+                SqlConnection conn = new SqlConnection(cs);
+                string sql = @"SELECT * 
+                            FROM OrderedItem O JOIN Product P ON O.ProductId = P.ProductId                            
+                            WHERE OrderId = @ID";
+
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@ID", "12345");
+                conn.Open();
+
+                DataSet ds = new DataSet();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(ds);
+                rptOrdered.DataSource = ds;
+                rptOrdered.DataBind();
+                conn.Close();
             }
         }
     }
